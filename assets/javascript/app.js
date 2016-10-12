df3a780f5cc231d859c2d26804d9dfd43fb5a8b5
Jae Gomez <emailjuliogomez@gmail.com>
2016-10-12 14:18:45 -0400

    jQuery show hides working on slideshows to show

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+
+	//originally hiding all carousel for sections from showing
+	$("#kitchens").click(function(){
+		console.log('succes')
+		$("#myCarousel").hide();
+		$(".textUnderCarousel").hide();
+		$(".border").hide();
+
+	});
+
+});
